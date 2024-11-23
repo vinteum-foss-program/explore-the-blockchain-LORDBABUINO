58114834b7e998680dfed0174e95de1558446195
LORDBABUINO <jorge.x7@gmail.com>
2024-11-23 00:42:10 -0300

    removes sensive data XD

--- submission/003.sh
+++ submission/003.sh
@@ -1,9 +1,9 @@
 # How many new outputs were created by block 123,456?
-transactions=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_100 -rpcpassword=QGEExJ6CHJUs getblock $(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_100 -rpcpassword=QGEExJ6CHJUs getblockhash 123456) | jq -r '.tx[]')
+transactions=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 123456) | jq -r '.tx[]')
 
 total_count=0
 for txid in $transactions; do
-  output=$(bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_100 -rpcpassword=QGEExJ6CHJUs getrawtransaction "$txid" true | jq '.vout | length')
+  output=$(bitcoin-cli getrawtransaction "$txid" true | jq '.vout | length')
   total_count=$((total_count + output))
 done
 
