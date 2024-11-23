2377797d8a6e0834e9049d2e6db7f2d9551f8550
LORDBABUINO <jorge.x7@gmail.com>
2024-11-23 00:45:06 -0300

    submition 004

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,8 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+descriptor="tr($xpub/100)"
+checksum=$(bitcoin-cli getdescriptorinfo $descriptor | jq -r .checksum)
+descriptor="$descriptor#$checksum"
+bitcoin-cli deriveaddresses "$descriptor" | jq -r '.[0]'
